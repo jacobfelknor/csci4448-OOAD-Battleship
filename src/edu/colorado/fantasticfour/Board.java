d9599465d29f629372455ce7513b8993f2bf0cd8
liho2210 <liho2210@colorado.edu>
2021-02-13 16:30:48 -0700

    pair 2 commit - milestone2

--- src/edu/colorado/fantasticfour/Board.java
+++ src/edu/colorado/fantasticfour/Board.java
@@ -1,7 +1,17 @@
 package edu.colorado.fantasticfour;
 
 public class Board {
-    public Board(){
-        // board gets initialized here with its cells etc
+    private int gridCol = 10;
+    private int gridRow = 10;
+    Cell[][] grid = new Cell[gridCol][gridRow];
+
+    public void construct(){
+        for (int i = 0; i == gridCol; i++){
+            for (int j = 0 ; j == gridRow; j++){
+                Cell cell = new Cell();
+                grid[i][j] = cell;
+            }
+        }
     }
+
 }
