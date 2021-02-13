1e57ee42bfd48f3bced0d33170463982cc87ce67
Jacob Felknor <jacobfelknor073@gmail.com>
2021-02-13 10:03:53 -0700

    got testing set up

---
+++ test/GameClassTest.java
@@ -0,0 +1,12 @@
+import edu.colorado.fantasticfour.*;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class GameClassTest {
+
+    @Test
+    public void canCreateGame() {
+        Game game = new Game();
+    }
+
+}
