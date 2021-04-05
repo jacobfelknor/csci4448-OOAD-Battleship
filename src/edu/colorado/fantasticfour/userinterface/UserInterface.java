2a1fb6e49c35e84d4cdcf2fa3a92c49ab3e2623a
atdh <dhungelatul@yahoo.com>
2021-04-05 11:59:01 -0600

    userinterface started

---
+++ src/edu/colorado/fantasticfour/userinterface/UserInterface.java
@@ -0,0 +1,14 @@
+package edu.colorado.fantasticfour.userinterface;
+import java.awt.*;
+import javax.swing.*;
+
+public class UserInterface {
+    JFrame s = new JFrame("My title goes here");
+
+    s.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    //s.add(ui);
+    s.setSize(500,500);
+    s.setVisible(true);
+}
+
+
